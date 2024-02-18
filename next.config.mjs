c2de2b3b736b510ec0810f677370d0372928f2d8
Wayne Myo <waiyankm93@gmail.com>
2024-02-18 09:16:42 +0800

    Update configs to deploy on AWS Amplify instead of S3 Static Hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Uncomment this to export static web output for hosting
+    // output: 'export', // Uncomment this to export static web output for hosting
 };
 
 export default nextConfig;
